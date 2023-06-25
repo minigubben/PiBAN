967f29ad634c10b412a67af702f3bf439449f4ac
vsiupdate <120747960+vsiupdate@users.noreply.github.com>
2023-06-25 10:09:03 -0400

    Update install.sh
    Replace wiringpi with python3-gpiozero

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt update
-sudo apt -y install secure-delete wiringpi nwipe
-sudo touch /var/log/PiBAN.log
+sudo apt update  
+sudo apt -y install secure-delete python3-gpiozero nwipe  
+sudo touch /var/log/PiBAN.log  
 sudo ./update.sh
