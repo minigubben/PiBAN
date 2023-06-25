e9af6d6e7162620b7d32d7b85c7fe90e178527bf
vsiupdate <120747960+vsiupdate@users.noreply.github.com>
2023-06-25 17:29:51 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
-rm /usr/local/bin/nuke.sh &>/dev/null
-rm /usr/local/bin/usbmount.sh &>/dev/null
-rm /etc/udev/rules.d/usbmount.rules &>/dev/null
+#!/bin/bash
 
-cp nuke.sh /usr/local/bin/nuke.sh
-cp usbmount.sh /usr/local/bin/usbmount.sh
-cp usbmount.rules /etc/udev/rules.d/usbmount.rules
+sudo rm /usr/local/bin/nuke.sh &>/dev/null
+sudo rm /usr/local/bin/usbmount.sh &>/dev/null
+sudo rm /etc/udev/rules.d/usbmount.rules &>/dev/null
 
-udevadm control --reload-rules
+sudo cp nuke.py /usr/local/bin/nuke.py
+sudo cp usbmount.sh /usr/local/bin/usbmount.sh
+sudo cp usbmount.rules /etc/udev/rules.d/usbmount.rules
 
-chmod +x /usr/local/bin/nuke.sh /usr/local/bin/usbmount.sh
+sudo chmod +x /usr/local/bin/nuke.py /usr/local/bin/usbmount.sh
+
+sudo udevadm control --reload-rules
