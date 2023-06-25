365205c761e0e61a1d5111c0eb75c33399232a7c
vsiupdate <120747960+vsiupdate@users.noreply.github.com>
2023-06-25 10:07:41 -0400

    Update nuke.sh
    Wiringpi package is deprecated, updated with python script

--- nuke.sh
+++ nuke.sh
@@ -1,75 +1,67 @@
-#!/bin/bash
-devname=$(basename $1)
+#!/usr/bin/python3
 
-echo "NUKING $1" >> /var/log/PiBAN.log
-#TURN ON LED
-gpio -g mode 17 out
-gpio -g write 17 1
+from gpiozero import LED, Button
+import os
+import sys
+import subprocess
 
-# This next line handles securely erasing the disk.
-# Pick one. Or none if you don't need secure erase.
-# 1 Pass. (Fastest)
-shred -v --iterations=1 "$1"
-# This will run a DOD Short erase(3 passes)(Slow)
-#nwipe --autonuke --nogui --nowait "$1"
-# DOD 5220.22-M (7 Passes)(Just use a hammer instead)
-#nwipe --autonuke --nogui --nowait --method=dod "$1"
+# If the switch connected to GPIO 22 is off, (button_select.is_pressed is False) 
+# when the script starts, it will run the shred command. Else, it will run the nwipe command. 
+# GPIO 17 is the "shred" notification LED.  It will be ON during shred/nwipe operation.
+# GPIO 27 is the "ready" LED.  It will be OFF during shred/nwipe, ON at all other times.
 
-# Now that We're erased, we can do whatever we want with our new drive.
-# There are a couple of options here, but by default, we'll just create a new
-# partition and format it to FAT32.
-# Another option would be to dd an image onto it.
-#dd if=/path/to/file.img of="$1" bs=512
-# If you decide to go this route, make sure to comment out every line before
-# sync or it will overrite your image!
+led_process = LED(17)
+led_ready = LED(27)
+button_select = Button(22)
 
-#The following ugly mess creates a new partition table with one partition that takes up the whole disk.
-echo "o
-n
-p
+devname = os.path.basename(sys.argv[1])
 
+#-----------------------------------------------------------------------------
+# Based on switch on/off, automatically run 1-Pass or 3-Pass
+#-----------------------------------------------------------------------------
+led_ready.on()
 
+with open('/var/log/PiBAN.log', 'a') as log_file:
+    log_file.write(f'NUKING {sys.argv[1]}\n')
 
-w
+led_process.on()
+led_ready.off()
 
-" | fdisk "$1"
-# This puts our new filesystem onto the first partition on the disk.
-# Fat32 by default. Uncomment only one of the following lines:
-mkfs.vfat -F 32 "$1"1
-# NTFS. Install the package ntfs-3g to use this option!
-#mkfs.ntfs -F "$1"1
-#mkfs.ext3 -F "$1"1
-#mkfs.ext2 -F "$1"1
+# If the switch is pressed (Button returns False when pressed), run shred
+if not button_select.is_pressed:
+    subprocess.run(['shred', '-v', '--iterations=1', sys.argv[1]])
+else:
+    subprocess.run(['nwipe', '--autonuke', '--nogui', '--nowait', sys.argv[1]])
 
-# This next section is for doing things with the filesystem once it's been created.
-# Mount our new fs to a folder with the same name as the device(to prevent confilicts with
-# other instances of this script).
-mntpath=/mnt/$(basename "$1")1
-mkdir $mntpath
-mount "$1"1 $mntpath
-cd $mntpath
+led_process.off()
+#-----------------------------------------------------------------------------
+# Format drive with FAT32
+#-----------------------------------------------------------------------------
+subprocess.run(['parted', '-s', sys.argv[1], 'mklabel', 'msdos'])
+subprocess.run(['parted', '-s', sys.argv[1], 'mkpart', 'primary', 'fat32', '0%', '100%'])
+subprocess.run(['mkfs.vfat', '-F', '32', f'{sys.argv[1]}1'])
 
-# Now we have a couple of options of what to do. By default we'll create a text file
-# to inform the user that the script worked.
-touch Erased_With_PiBAN.txt
-echo -e "This drive has been securely erased and repartitioned with PiBAN\n\
-https://github.com/Real-Time-Kodi/PiBAN" > Erased_With_PiBAN.txt
-#Let's also copy our log file to the device.
-cp /tmp/$devname.log .
+mntpath = f'/mnt/{os.path.basename(sys.argv[1])}1'
+os.makedirs(mntpath, exist_ok=True)
 
-# We could also take this oppurtunity to call another script:
-#/path/to/script
-# Or we could copy some files to our new partition:
-#cp -R /path/to/files .
+subprocess.run(['mount', f'{sys.argv[1]}1', mntpath])
 
-cd /
-umount $mntpath
-rmdir $mntpath
+os.chdir(mntpath)
 
-sync #SYNC because I don't trust the kernel to do it for me.
+with open('Erased_With_PiBAN.txt', 'w') as txt_file:
+    txt_file.write('This drive has been securely erased and repartitioned with PiBAN\n'
+                   'https://github.com/Real-Time-Kodi/PiBAN')
 
-#TURN OFF LED
-gpio -g write 17 0
+subprocess.run(['cp', f'/tmp/{devname}.log', '.'])
 
-echo "Drive Completed $1" >> /var/log/PiBAN.log
+os.chdir('/')
 
+subprocess.run(['umount', mntpath])
+os.rmdir(mntpath)
+#-----------------------------------------------------------------------------
+subprocess.run(['sync'])
+
+led_ready.on()
+
+with open('/var/log/PiBAN.log', 'a') as log_file:
+    log_file.write(f'Drive Completed {sys.argv[1]}\n')
