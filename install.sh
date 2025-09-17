d5a73c1bccb423ea3799bf6b6c95a093933babbe
Noah Christiansson <noah@extronic.se>
2025-09-17 14:06:32 +0200

    Fix preemtive exit of nuke

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt update
-sudo apt -y install secure-delete nwipe python3-gpiozero python3-pip
-pip3 install RPi.GPIO
+sudo apt -y install secure-delete nwipe python3-gpiozero python3-pip python3-RPi.GPIO
 sudo touch /var/log/PiBAN.log
 sudo ./update.sh
--- update.sh
+++ update.sh
@@ -5,9 +5,8 @@ sudo rm /usr/local/bin/usbmount.sh &>/dev/null
 sudo rm /etc/udev/rules.d/usbmount.rules &>/dev/null
 
 sudo cp nuke.py /usr/local/bin/nuke.py
-sudo cp usbmount.sh /usr/local/bin/usbmount.sh
 sudo cp usbmount.rules /etc/udev/rules.d/usbmount.rules
 
-sudo chmod +x /usr/local/bin/nuke.py /usr/local/bin/usbmount.sh
+sudo chmod +x /usr/local/bin/nuke.py
 
 sudo udevadm control --reload-rules
